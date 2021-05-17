6908adfbe608ee53eb724b951e55ed56ee933dec
Giuseppe Di Palma <dipalmagiuseppe1@gmail.com>
2021-05-17 21:08:50 +0200

    some fix

--- LabelPropagation-C++/hyper_graph_gen.cpp
+++ LabelPropagation-C++/hyper_graph_gen.cpp
@@ -2,10 +2,10 @@
 
 #define MAXITER 100
 #define SEED 1234
-#define VERTEX_N 1000    //100
-#define HYPER_EDGES_N 60 //20
+#define VERTEX_N 20000    //100
+#define HYPER_EDGES_N 1000 //20
 
-int main()
+int main(int argc, char *argv[])
 {
     MT::MersenneTwist rng;
     rng.init_genrand(SEED);
