3e4e2dec019158e6189f44b57c2b94faa64c7921
Giuseppe Di Palma <dipalmagiuseppe1@gmail.com>
2021-05-24 09:40:14 +0200

    fix for testing

--- LabelPropagation-Julia/src/run.jl
+++ LabelPropagation-Julia/src/run.jl
@@ -20,12 +20,18 @@ for v in size_v
     for e in size_e
         for p in p_list
             h = read_hypergraph("../LabelPropagation-C++/resources/h_test_hypergraph_$(v)_$(e)_$(p)00000.txt")
+            result_files = open("results_file/results_julia_$(v)_$(e)_$(p).txt", "w+")
+            #h = read_hypergraph("../LabelPropagation-C++/resources/h_test_hypergraph_5000_300_0.300000.txt")
             base = 0
             for i in 1:5
                 base += @elapsed(our_findcommunities(h, cflp))
+                write(result_files,"Results with vertex [$v], with edges [$e] and density [$p]\n")
+                write(result_files, "Find Communities time: $(base)\n")
+                base = 0
             end
-            base /= 5 #aritmetic mean
-            write(io,"$(v) - $(e) - $(p) : $(base)\n")
+            #base /= 5 #aritmetic mean
+            #write(io,"$(v) - $(e) - $(p) : $(base)\n")
+            close(result_files)
         end
     end
 end
@@ -33,4 +39,4 @@ end
 close(io)
 
 
-comms[:vlabels]
+# comms[:vlabels]
